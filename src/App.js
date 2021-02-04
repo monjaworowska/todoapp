2ce74089e29029ba3b5e7cb11ef57052c36e0abc
Monika Jaworowska <mon.jaworowska@gmail.com>
2021-02-04 16:25:33 +0100

    Input field added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 
 const App = ({ todos }) => {
+  const [task, setTask] = useState("");
   return (
     <div>
       <h1>To Do App Redux Template</h1>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input value={task} onChange={(e) => setTask(e.target.value)} />
+        <button>Add</button>
+      </form>
       {!todos
         ? []
         : todos.map((todo) => {
