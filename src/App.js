2b14f104fbc1f6ea01863fb3380e92c868fb3065
Monika Jaworowska <mon.jaworowska@gmail.com>
2021-02-04 16:55:35 +0100

    Test number added

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = ({ todos }) => {
   return (
     <div>
       <h1>To Do App Redux Template</h1>
-      <p>Test branch</p>
+      <p>Test branch 1</p>
       <form onSubmit={(e) => e.preventDefault()}>
         <input value={task} onChange={(e) => setTask(e.target.value)} />
         <button>Add</button>
