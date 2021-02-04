9c17566a688f124dd215dbc4ceeef4159f659215
Monika Jaworowska <mon.jaworowska@gmail.com>
2021-02-04 14:17:35 +0100

    Updates

---
+++ static/js/main.0727d3f5.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonptodoapp=this.webpackJsonptodoapp||[]).push([[0],{9:function(t,e,n){"use strict";n.r(e);var o=n(1),c=n.n(o),r=n(3),p=n.n(r),s=n(0);var a=function(){return Object(s.jsx)("h1",{children:"To Do App"})};p.a.render(Object(s.jsx)(c.a.StrictMode,{children:Object(s.jsx)(a,{})}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.0727d3f5.chunk.js.map
