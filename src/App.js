123bf478777fe77d0ab9fec908b930abedfc91ed
Monika Jaworowska <mon.jaworowska@gmail.com>
2021-02-04 15:23:53 +0100

    Reducer created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 function App() {
-  return <h1>To Do App</h1>;
+  return <h1>To Do App Redux Template</h1>;
 }
 
 export default App;
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,11 @@
+const initialState = [
+  { id: 0, task: "Taks 1", done: false },
+  { id: 1, task: "Task 2", done: false },
+  { id: 2, task: "Task 3", done: false },
+];
+
+const reducer = (state = initialState, action) => {
+  return state;
+};
+
+export default reducer;
